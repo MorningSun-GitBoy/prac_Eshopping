2b744d4590ce38a774edd52efeef3360e65a8ac6
Bachelor <AndyWolfCF2013@163.com>
2020-07-27 17:57:33 +0800

    test framework

--- Eshopping/src/main/java/com/newshopping/controller/TestCon.java
+++ Eshopping/src/main/java/com/newshopping/controller/TestCon.java
@@ -1,12 +1,13 @@
 package com.newshopping.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 //使用此注解可以在SpringMVC中被扫描到
 public class TestCon {
-
+    @RequestMapping("/test.do")
     public String test() {
-	return "";
+	return "test";//返回的字符串是页面的名字
     }
 }
