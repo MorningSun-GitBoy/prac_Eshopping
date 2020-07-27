0a6e21601a5d887521009b12c29d686a43d8f5c4
Bachelor <AndyWolfCF2013@163.com>
2020-07-27 15:31:13 +0800

    configur over

---
+++ Eshopping/src/main/java/com/newshopping/controller/TestCon.java
@@ -0,0 +1,12 @@
+package com.newshopping.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+//使用此注解可以在SpringMVC中被扫描到
+public class TestCon {
+
+    public String test() {
+	return "";
+    }
+}
