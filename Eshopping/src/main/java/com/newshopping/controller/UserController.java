2b09230540932bce922da74d4002717c4e2b3c39
Bachelor <AndyWolfCF2013@163.com>
2020-08-07 15:59:39 +0800

    creat tables in database and add new denpendencies

--- Eshopping/src/main/java/com/newshopping/controller/UserController.java
+++ Eshopping/src/main/java/com/newshopping/controller/UserController.java
@@ -1,11 +1,24 @@
 package com.newshopping.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.newshopping.entity.User;
 import com.newshopping.service.IUserService;
 
+@RestController
 public class UserController {
+    
     @Autowired
     private IUserService userService;
     
+    @RequestMapping("/users.do")
+    public List<User> queryAll() {
+	List<User> queryList = userService.queryAllUsers();
+	System.out.println(queryList);
+	return queryList;
+    }
 }
